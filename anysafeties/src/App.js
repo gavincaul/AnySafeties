5819e31a4694f6cce74c5e2c9861b58b410d4f79
Gavin Caulfield <gavcaul@udel.edu>
2023-06-26 22:18:56 -0400

    uploaded websiote

--- anysafeties/src/App.js
+++ anysafeties/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Landing from "anysafeties\src\pages\Landing.tsx";
+import AboutUs from "anysafeties\src\pages\About.tsx";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+          <Route path="/" element={<Landing />}></Route>
+          <Route path="/About" element={<AboutUs />}></Route>
+      </Routes>
   );
 }
-
-export default App;
--- anysafeties/src/pages/Landing.tsx
+++ anysafeties/src/pages/Landing.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import "./CustomerStyles.css";
 
 export default function Landing() {
     return (
