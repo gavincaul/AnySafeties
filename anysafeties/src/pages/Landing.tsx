33c3474b1e827c0462cc9e07f104d623a0eea75b
Gavin Caulfield <gavcaul@udel.edu>
2023-06-26 21:40:58 -0400

    made website

---
+++ anysafeties/src/pages/Landing.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import "./CustomerStyles.css";
+
+export default function Landing() {
+    return (
+        <>
+            <NavBar></NavBar>
+            Welcome to my page
+        </>
+    );
+}
